c7da2e44fd566f94a486c385f8f573f2ab098357
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-02 12:47:11 +0100

    Cleanup

--- users/bas.nix
+++ users/bas.nix
@@ -18,8 +18,8 @@
 
   age.secrets."dotfiles-deploy-key" = {
     file = ../secrets/dotfiles-deploy-key.age;
-    owner = "bas";
-    group = "bas";
+    owner = "root";
+    group = "wheel";
     mode = "0440";
   };
 
