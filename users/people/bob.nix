f6fb0f6c9f17b1125ffd78d46a6d3dc40c5d7cde
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-03 21:04:47 +0100

    Progress

--- users/people/bob.nix
+++ users/people/bob.nix
@@ -2,14 +2,7 @@
   uid = 1001;
   groups = [ "wheel" ];
   shell = "zsh";
-  sshPubKeyFile = ../keys/bob.pub;
+  sshPubKeyFile = ../keys/bas.pub;
 
-  roles = {
-    # dev.enable = true;
-    # dotfiles = {
-    #   enable = true;
-    #   sparse = [ "nvim" ];
-    #   linkMap = { ".config/nvim" = "nvim/.config/nvim"; };
-    # };
-  };
+  roles = { };
 }
