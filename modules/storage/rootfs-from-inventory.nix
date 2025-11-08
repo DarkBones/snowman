4cf75470cf4697abb3bdd4b10270e3e1abc362b0
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-08 12:52:11 +0100

    Convert storage

--- modules/storage/rootfs-from-inventory.nix
+++ modules/storage/rootfs-from-inventory.nix
@@ -1,13 +1,17 @@
 { inv, currentHost, lib, ... }:
 let
-  host = inv.hosts.${currentHost};
-  fs = host.hardware.fs;
-  diskoOn = host.provision.disk.enable or false;
-in lib.mkIf (!diskoOn) {
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/${fs.rootLabel}";
-    fsType = fs.type;
-  };
-
-  boot.supportedFilesystems = lib.mkIf (fs.type == "btrfs") [ "btrfs" ];
-}
+  hasHost = builtins.hasAttr currentHost inv.hosts;
+  cfg = if !hasHost then
+    { }
+  else
+    let
+      host = inv.hosts.${currentHost};
+      fs = host.hardware.fs;
+      diskoOn = host.provision.disk.enable or false;
+    in lib.mkIf (!diskoOn) {
+      fileSystems."/" = {
+        device = "/dev/disk/by-label/${fs.rootLabel}";
+        fsType = fs.type;
+      };
+    };
+in { } // cfg
