62b47ed6b0b9536e1b6b1075151d269de1e5013a
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-08 12:56:16 +0100

    Convert home

--- modules/home/from-inventory.nix
+++ modules/home/from-inventory.nix
@@ -1,23 +1,23 @@
 { inv, currentHost, lib, ... }:
 let
   hasHost = builtins.hasAttr currentHost inv.hosts;
-  hostUsers = if hasHost then inv.hosts.${currentHost}.users else [ ];
-  selected =
-    lib.filterAttrs (n: u: lib.elem n hostUsers && (u.homeManaged or false))
-    inv.users;
-in {
-  assertions = [{
-    assertion = hasHost;
-    message = "Inventory: host ${currentHost} not found in inv.hosts";
-  }];
-
-  home-manager.users = lib.genAttrs (builtins.attrNames selected) (name: {
-    imports = [ ./default.nix ];
-    home.username = name;
-    home.homeDirectory = "/home/${name}";
-    home.stateVersion = inv.release;
-    programs.home-manager.enable = true;
-    systemd.user.startServices = false;
-    roles = selected.${name}.roles or { };
-  });
-}
+  cfg = if !hasHost then
+    { }
+  else
+    let
+      hostUsers = if hasHost then inv.hosts.${currentHost}.users else [ ];
+      selected =
+        lib.filterAttrs (n: u: lib.elem n hostUsers && (u.homeManaged or false))
+        inv.users;
+    in {
+      home-manager.users = lib.genAttrs (builtins.attrNames selected) (name: {
+        imports = [ ./default.nix ];
+        home.username = name;
+        home.homeDirectory = "/home/${name}";
+        home.stateVersion = inv.release;
+        programs.home-manager.enable = true;
+        systemd.user.startServices = false;
+        roles = selected.${name}.roles or { };
+      });
+    };
+in { } // cfg
