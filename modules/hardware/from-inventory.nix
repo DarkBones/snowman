d669cfcfc0691df989268fa68df5da5b02bd74db
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-10 19:47:09 +0100

    Cleanup

--- modules/hardware/from-inventory.nix
+++ modules/hardware/from-inventory.nix
@@ -1,84 +1,54 @@
 { inv, lib, currentHost, ... }:
+
 let
   hasHost = builtins.hasAttr currentHost inv.hosts;
-  cfg = if !hasHost then
-    { }
-  else
-    let
-      host = inv.hosts.${currentHost};
-      fw = host.hardware.boot.firmware or "bios";
-      loader = if fw == "efi" then "systemd-boot" else "grub";
-      espSz = host.hardware.boot.espSize or "512MiB";
-    in {
-      networking.hostName = host.hostname or currentHost;
-      networking.useDHCP = host.useDHCP or true;
-      system.stateVersion = inv.release;
+  host = if hasHost then inv.hosts.${currentHost} else { };
 
-      # Bootloader
-      boot.loader.grub = lib.mkMerge [
-        (lib.mkIf (fw == "bios") {
-          enable = true;
-          mirroredBoots = [{
-            devices = [ host.hardware.disk.device ];
-            path = "/";
-          }];
-          devices = lib.mkForce [ ];
-        })
-        (lib.mkIf (fw == "efi" && loader == "grub") {
-          enable = true;
-          efiSupport = true;
-          efiInstallAsRemovable = true;
-          devices = lib.mkForce [ ];
-        })
-      ];
+  diskoOn = hasHost && (host.provision.disk.enable or false);
 
-      # EFI -> pick loader
-      boot.loader.systemd-boot.enable =
-        (fw == "efi" && loader == "systemd-boot");
+  baseCfg = lib.mkIf hasHost {
+    # Safety: inventory must contain this host
+    assertions = [{
+      assertion = hasHost;
+      message = "Inventory: host ${currentHost} not found in inv.hosts";
+    }];
 
-      boot.loader.efi = lib.mkIf (fw == "efi") {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot";
-      };
+    networking.hostName = host.hostname or currentHost;
+    networking.useDHCP = host.useDHCP or true;
 
-      disko.devices.disk.main = lib.mkIf host.provision.disk.enable {
-        type = "disk";
-        device = host.hardware.disk.device;
-        content = {
-          type = "gpt";
-          partitions = (lib.optionalAttrs (fw == "bios") {
-            bios = {
-              size = "1M";
-              type = "EF02";
-            };
-          }) // (lib.optionalAttrs (fw == "efi") {
-            esp = {
-              size = espSz;
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-          }) // {
-            root = {
-              size = "100%";
-              type = "8300";
-              content = {
-                type = "filesystem";
-                format = host.hardware.fs.type; # "btrfs" | "ext4" | …
-                extraArgs = [ "-L" host.hardware.fs.rootLabel ];
-                mountpoint = "/";
-              };
+    # Tie system.stateVersion to your inventory release
+    system.stateVersion = inv.release;
+
+    # Simple BIOS GRUB on a single disk (your VM)
+    boot.loader.grub = {
+      enable = true;
+      devices = lib.mkForce [ host.hardware.disk.device ];
+    };
+
+    # Explicitly keep systemd-boot off for now
+    boot.loader.systemd-boot.enable = false;
+  };
+
+  diskoCfg = lib.mkIf diskoOn {
+    disko.devices.disk.main = {
+      type = "disk";
+      device = host.hardware.disk.device;
+      content = {
+        type = "gpt";
+        partitions = {
+          root = {
+            size = "100%";
+            type = "8300";
+            content = {
+              type = "filesystem";
+              format = host.hardware.fs.type; # "btrfs" | "ext4" | …
+              extraArgs = [ "-L" host.hardware.fs.rootLabel ];
+              mountpoint = "/";
             };
           };
         };
       };
     };
-in {
-  assertions = [{
-    assertion = hasHost;
-    message = "Inventory: host ${currentHost} not found in inv.hosts";
-  }];
-} // cfg
+  };
+
+in { config = lib.mkMerge [ baseCfg diskoCfg ]; }
