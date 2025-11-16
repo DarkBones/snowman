7d7cc3a319ea5d3422805bce758d919bbc65af4b
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-16 15:38:12 +0100

    Cleanup

--- modules/bootstrap-usb.nix
+++ modules/bootstrap-usb.nix
@@ -4,13 +4,11 @@ let
   host = if hasHost then inv.hosts.${currentHost} else { };
   usbCfg = host.bootstrap.usb or { enable = false; };
 in {
-  config = {
-    fileSystems = lib.optionalAttrs usbCfg.enable {
-      "${usbCfg.path}" = {
-        device = "/dev/disk/by-label/${usbCfg.label}";
-        fsType = usbCfg.fsType or "vfat";
-        options = [ "nofail" ];
-      };
+  config = lib.mkIf (hasHost && usbCfg.enable) {
+    fileSystems.${usbCfg.path} = {
+      device = "/dev/disk/by-label/${usbCfg.label}";
+      fsType = usbCfg.fsType or "vfat";
+      options = [ "nofail" ];
     };
   };
 }
