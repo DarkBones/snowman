da8c2e76d93fa6f40af978c5ac282521b7a03d17
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-17 19:17:50 +0100

    Make hardware optional

--- modules/hardware/boot-from-inventory.nix
+++ modules/hardware/boot-from-inventory.nix
@@ -2,25 +2,31 @@
 let
   hasHost = builtins.hasAttr currentHost inv.hosts;
   host = if hasHost then inv.hosts.${currentHost} else { };
-  hw = host.hardware or { };
+
+  hasHw = hasHost && host ? hardware;
+  hw = if hasHw then host.hardware else { };
   boot = hw.boot or { };
-  fw = boot.firmware or "bios"; # "bios" | "efi"
+
+  # "bios" | "efi" | "none" | null
+  fw = boot.firmware or null;
+
   dev = hw.bootDevice or null;
 in {
-  config = lib.mkIf hasHost (lib.mkMerge [
-    # BIOS: use GRUB on the bootDevice (or /dev/vda as a sane VM default)
-    (lib.mkIf (fw == "bios") {
-      boot.loader.grub = {
-        enable = true;
-        devices = [ (if dev != null then dev else "/dev/vda") ];
-        useOSProber = false;
-      };
-    })
+  config = lib.mkIf (hasHost && hasHw && fw != null && fw != "none")
+    (lib.mkMerge [
+      # BIOS: use GRUB on the bootDevice (or /dev/vda as a sane VM default)
+      (lib.mkIf (fw == "bios") {
+        boot.loader.grub = {
+          enable = true;
+          devices = [ (if dev != null then dev else "/dev/vda") ];
+          useOSProber = false;
+        };
+      })
 
-    # EFI: use systemd-boot
-    (lib.mkIf (fw == "efi") {
-      boot.loader.systemd-boot.enable = true;
-      boot.loader.efi = { canTouchEfiVariables = true; };
-    })
-  ]);
+      # EFI: use systemd-boot
+      (lib.mkIf (fw == "efi") {
+        boot.loader.systemd-boot.enable = true;
+        boot.loader.efi = { canTouchEfiVariables = true; };
+      })
+    ]);
 }
