d2b1f640b1371c74fc77de9ba96c1d37567e5fa1
DarkBones <23454780+DarkBones@users.noreply.github.com>
2025-11-17 20:06:56 +0100

    Make hardware config optional

--- modules/hardware/boot-from-inventory.nix
+++ modules/hardware/boot-from-inventory.nix
@@ -2,31 +2,32 @@
 let
   hasHost = builtins.hasAttr currentHost inv.hosts;
   host = if hasHost then inv.hosts.${currentHost} else { };
-
-  hasHw = hasHost && host ? hardware;
-  hw = if hasHw then host.hardware else { };
-  boot = hw.boot or { };
-
-  # "bios" | "efi" | "none" | null
+  hasHw = hasHost && (host ? hardware);
+  boot = if hasHw then host.hardware.boot else { };
   fw = boot.firmware or null;
-
-  dev = hw.bootDevice or null;
 in {
-  config = lib.mkIf (hasHost && hasHw && fw != null && fw != "none")
-    (lib.mkMerge [
-      # BIOS: use GRUB on the bootDevice (or /dev/vda as a sane VM default)
-      (lib.mkIf (fw == "bios") {
-        boot.loader.grub = {
-          enable = true;
-          devices = [ (if dev != null then dev else "/dev/vda") ];
-          useOSProber = false;
-        };
-      })
+  config = lib.mkMerge [
+    # If the user provides *no* hardware block, disable ALL bootloaders.
+    (lib.mkIf (!hasHw) {
+      boot.loader.grub.enable = lib.mkForce false;
+      boot.loader.systemd-boot.enable = lib.mkForce false;
+    })
 
-      # EFI: use systemd-boot
-      (lib.mkIf (fw == "efi") {
-        boot.loader.systemd-boot.enable = true;
-        boot.loader.efi = { canTouchEfiVariables = true; };
-      })
-    ]);
+    # If firmware = "none", also disable bootloaders.
+    (lib.mkIf (fw == "none") {
+      boot.loader.grub.enable = lib.mkForce false;
+      boot.loader.systemd-boot.enable = lib.mkForce false;
+    })
+
+    # Normal BIOS/EFI handling...
+    (lib.mkIf (fw == "bios") {
+      boot.loader.grub.enable = true;
+      boot.loader.grub.devices = [ (host.hardware.bootDevice or "/dev/vda") ];
+    })
+
+    (lib.mkIf (fw == "efi") {
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
+    })
+  ];
 }
